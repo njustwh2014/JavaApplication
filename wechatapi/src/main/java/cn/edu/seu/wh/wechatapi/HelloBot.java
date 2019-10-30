4b084c667a9e5414afa052be947d0f461999871a
njustwh2014@163.com <njustwh2014@163.com>
2019-10-30 16:51:25 +0800

    2019/10/30

---
+++ wechatapi/src/main/java/cn/edu/seu/wh/wechatapi/HelloBot.java
@@ -0,0 +1,34 @@
+package cn.edu.seu.wh.wechatapi;
+
+import io.github.biezhi.wechat.WeChatBot;
+import io.github.biezhi.wechat.api.annotation.Bind;
+import io.github.biezhi.wechat.api.constant.Config;
+import io.github.biezhi.wechat.api.enums.MsgType;
+import io.github.biezhi.wechat.api.model.WeChatMessage;
+import io.github.biezhi.wechat.utils.StringUtils;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @program:JavaApplication
+ * @description:demo of wechatbot
+ * @author: Huan Wang(https://github.com/njustwh2014)
+ * @create:2019-10-30 15:39
+ **/
+@Slf4j
+public class HelloBot extends WeChatBot {
+    public HelloBot(Config config) {
+        super(config);
+    }
+
+    @Bind(msgType = MsgType.TEXT)
+    public void handleText(WeChatMessage message) {
+        if (StringUtils.isNotEmpty(message.getName())) {
+            log.info("接收到 [{}] 的消息: {}", message.getName(), message.getText());
+            this.sendMsg(message.getFromUserName(), "自动回复: " + message.getText());
+        }
+    }
+
+    public static void main(String[] args) {
+        new HelloBot(Config.me().autoLogin(true).showTerminal(true)).start();
+    }
+}
